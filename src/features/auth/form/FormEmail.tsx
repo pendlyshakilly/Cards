d2527fb4de6198c7dbde9c1b55ad6a41088f1b7d
HlebSayanov <say4havi@gmail.com>
2023-05-03 10:50:10 +0300

    refactor_signIn

--- src/features/auth/form/FormEmail.tsx
+++ src/features/auth/form/FormEmail.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { Input, InputLabel, FormControl} from "@mui/material";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 
 const FormEmail = (props: any) => {
-  return (
-    <FormControl sx={{ m: 1, width: "347px" }} variant="standard">
-      <InputLabel htmlFor="component-helper">Email</InputLabel>
-      <Input
-        {...props.register("email", { required: true })}
-        id="component-helper"
-        defaultValue=""
-        aria-describedby="component-helper-text"
-        aria-invalid={props.errors.email ? "true" : "false"}
-      />
+		return (
+			<FormControl sx={{ m: 1, width: "347px" }} variant="standard">
+					<InputLabel htmlFor="component-helper">Email</InputLabel>
+					<Input
+						{...props.register("email", {
+								required: 'Title is required',
+								pattern: {
+										value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+										message:'Please enter a valid email'
+						}
+						})}
+						id="component-helper"
+						error={props.errors?.email}
+						defaultValue=""
+						aria-describedby="component-helper-text"
+					/>
 
-      {props.errors.email && <p role="alert" style={{color: 'red'}}>Title is required</p>}
-    </FormControl>
-  );
+					{props.errors?.email && <p role="alert" style={{color: 'red',margin:'0px'}}>{props.errors.email.message}</p>}
+
+			</FormControl>
+		);
 };
 
 export default FormEmail;
